62ba62d62652ca2b899f9a04d095c79e0ba733ee
maheshyewate212-cell <maheshyewate212@gmail.com>
2026-04-24 08:22:49 +0530

    Number Spelling Using Switch Case

--- Switch.java
+++ Switch.java
@@ -5,12 +5,15 @@ public class Switch{
 
 	public static void main(String[] args){
 	Scanner sc = new Scanner(System.in);
-	System.out.println("Enter digit");
+	System.out.print("Enter digit ");
 	int digit = sc.nextInt();
 	
-	if(digit>0 && digit <= 20)
-		System.out.println(" Number between 0 to 20 ");
+	int hundred = digit/100;
+	int temp = digit - hundred*100;
+	int tens = temp/10;
+	int unit= digit%10;
 	
+	if(digit>0 && digit <= 20){
 	switch(digit){
 		case 0 : System.out.println("ZERO");
 				 break;
@@ -32,34 +35,92 @@ public class Switch{
 				 break;		 
 		case 9 : System.out.println("NINE");
 				 break;	
-		case 10 : System.out.println("ONE ZERO");
+		case 10 : System.out.println("TEN");
 				 break;
-		case 11 : System.out.println("ONE ONE");
+		case 11 : System.out.println("ELEVEN");
 				 break;
-		case 12 : System.out.println(" ONE TWO");
+		case 12 : System.out.println("TWELVE");
 				 break;
-		case 13 : System.out.println("ONE THREE");
+		case 13 : System.out.println("THIRTEEN");
 				 break;
-	    case 14 : System.out.println("ONE FOUR");
+	    case 14 : System.out.println("FOURTEEN");
 				 break;
-		case 15 : System.out.println("ONE FIVE");
+		case 15 : System.out.println("FIFTEEN");
 				 break;
-		case 16 : System.out.println("ONE SIX");
+		case 16 : System.out.println("SIXTEEN");
 				 break;		 
-		case 17 : System.out.println("ONE SEVEN");
+		case 17 : System.out.println("SEVENTEEN");
 				 break;		 
-		case 18 : System.out.println("ONE EIGHT");
+		case 18 : System.out.println("EIGHTEEN");
 				 break;		 
-		case 19 : System.out.println("ONE NINE");
+		case 19 : System.out.println("NINETEEN");
 				 break;		
-		case 20 : System.out.println("TWO ZERO");
+		case 20 : System.out.println("TWENTY");
 				 break;
 		default : System.err.println("Loading ....");
                break;		
 	}
+	}
 	
-	if(digit > 10 && digit <=20)
-		switch(digit){
+	switch(hundred){
+		case 1 : System.out.print("ONE HUNDRED ");
+				 break;
+		case 2 : System.out.print("TWO HUNDRED ");
+				 break;
+		case 3 : System.out.print("THREE HUNDRED ");
+				 break;
+	    case 4 : System.out.print("FOUR HUNDRED ");
+				 break;
+		case 5 : System.out.print("FIVE HUNDRED ");
+				 break;
+		case 6 : System.out.print("SIX HUNDRED ");
+				 break;		 
+		case 7 : System.out.print("SEVEN HUNDRED ");
+				 break;		 
+		case 8 : System.out.print("EIGHT HUNDRED ");
+				 break;		 
+		case 9 : System.out.print("NINE HUNDRED ");
+				 break;	
+	}
+	
+		switch(tens){
+		case 2 : System.out.print("TWENTY ");
+				 break;
+		case 3 : System.out.print("THIRTY ");
+				 break;
+	    case 4 : System.out.print("FOURTY ");
+				 break;
+		case 5 : System.out.print("FIFTY ");
+				 break;
+		case 6 : System.out.print("SIXTY ");
+				 break;		 
+		case 7 : System.out.print("SEVENTY ");
+				 break;		 
+		case 8 : System.out.print("EIGHTY ");
+				 break;		 
+		case 9 : System.out.print("NINETY ");
+				 break;		
+		}
+		
+		switch(unit){
+		case 1 : System.out.print("ONE ");
+				 break;
+		case 2 : System.out.print("TWO ");
+				 break;
+		case 3 : System.out.print("THREE ");
+				 break;
+	    case 4 : System.out.print("FOUR ");
+				 break;
+		case 5 : System.out.print("FIVE ");
+				 break;
+		case 6 : System.out.print("SIX ");
+				 break;		 
+		case 7 : System.out.print("SEVEN ");
+				 break;		 
+		case 8 : System.out.print("EIGHT ");
+				 break;		 
+		case 9 : System.out.print("NINE ");
+				 break;	
 			
 		}
 		
